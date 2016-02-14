538cb51671cababf1bb7152b7c80afdfefd40755
dazinator <darrell.tunnell@googlemail.com>
2016-02-14 15:58:29 +0000

    closes #3 - added DotNetNuke.Core NuGet package adn Default.ascx

---
+++ src/DnnTestModule/Default.ascx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DotNetNuke.Entities.Modules;
+
+namespace DnnTestModule
+{
+    public partial class Default : PortalModuleBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
